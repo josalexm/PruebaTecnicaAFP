fc0052e8e5a5a2cebae65b40cd1a96b8e5d4c08c
Alexander Muñoz <jmunoz@bitworks.com.sv>
2020-11-25 12:34:26 -0600

    Definicion e implementacion de repositorio para afiliados

---
+++ PTEC.Core/Repositories/IAfiliadoRepository.cs
@@ -0,0 +1,38 @@
+﻿using PTEC.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTEC.Core.Repositories
+{
+    public interface IAfiliadoRepository
+    {
+        /// <summary>
+        /// Extrae la informacion completa de un afiliado mediante su identificador
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<Afiliado> GetAfiliadoByIdAsync(Guid id);
+
+        /// <summary>
+        /// Extrae la informacion completa de un afiliado mediante su numero de documento
+        /// </summary>
+        /// <param name="numeroDocumento"></param>
+        /// <returns></returns>
+        Task<Afiliado> GetAfiliadoByIdNumeroDocumento(string numeroDocumento);
+
+        /// <summary>
+        /// Agrega un nuevo registro a la base de datos
+        /// </summary>
+        /// <param name="afiliado"></param>
+        /// <returns></returns>
+        Task AddAfiliadoAsync(Afiliado afiliado);
+
+        /// <summary>
+        /// Hace efectivos los cambios realizados en la base de datos
+        /// </summary>
+        /// <returns></returns>
+        Task SaveChangesAsync();
+    }
+}
---
+++ PTEC.Data/Repositories/AfiliadoRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PTEC.Core.Models;
+using PTEC.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTEC.Data.Repositories
+{
+    public class AfiliadoRepository : IAfiliadoRepository
+    {
+        PTecnicaContext _db;
+
+        public AfiliadoRepository(PTecnicaContext db)
+        {
+            _db = db;
+        }
+
+        public async Task AddAfiliadoAsync(Afiliado afiliado)
+        {
+            await _db.AddAsync(afiliado);
+        }
+
+        public async Task<Afiliado> GetAfiliadoByIdAsync(Guid id)
+        {
+            return await _db.Afiliado.Include(x => x.Telefonos).SingleOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<Afiliado> GetAfiliadoByIdNumeroDocumento(string numeroDocumento)
+        {
+            return await _db.Afiliado.AsNoTracking().Include(x => x.Telefonos).SingleOrDefaultAsync(x => x.NumeroDocumento == numeroDocumento);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
