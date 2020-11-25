f5d02c8cfb07327faa6fe41e04d5a9b582412f36
Alexander Muñoz <jmunoz@bitworks.com.sv>
2020-11-25 12:09:59 -0600

    Creacion de modelado

---
+++ PTEC.Core/Models/Afiliado.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PTEC.Core.Models
+{
+    public class Afiliado
+    {
+        /// <summary>
+        /// Identificador del afiliado
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Nombre del afiliado
+        /// </summary>
+        public string Nombre { get; set; }
+
+        /// <summary>
+        /// Apellido del afiliado
+        /// </summary>
+        public string Apellido { get; set; }
+
+        /// <summary>
+        /// Numero de identificadion del afiliado
+        /// </summary>
+        public string NumeroDocumento { get; set; }
+
+        /// <summary>
+        /// Fecha de ingreso del registro
+        /// </summary>
+        public DateTimeOffset FechaIngreso { get; set; }
+
+        /// <summary>
+        /// Lista de telefonos pertenecientes al afiliado
+        /// </summary>
+        public List<Telefono> Telefonos { get; set; }
+    }
+}
---
+++ PTEC.Core/Models/Telefono.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PTEC.Core.Models
+{
+    public class Telefono
+    {
+        /// <summary>
+        /// Identificador del telefono
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Numero de telefono
+        /// </summary>
+        public string Numero { get; set; }
+
+        /// <summary>
+        /// Identificado del afiliado al que pertenece el telefono
+        /// </summary>
+        public Guid IdAfiliado { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Afiliado Afiliado { get; set; }
+    }
+}
